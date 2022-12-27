9d5b417b485260e43017bbf1a87e88335d6e8852
rikiya miyazawa <rky.weed.rss@gmail.com>
2022-12-27 11:34:41 +0900

    feat:削除機能を作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i(show edit update)
+  before_action :set_task, only: %i(show edit update destroy)
   def index
     @tasks = Task.all
   end
@@ -32,6 +32,8 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    @task.destroy
+    redirect_to tasks_path, notice: 'タスクを削除しました'
   end
 
   private
