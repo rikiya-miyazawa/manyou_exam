6e1a495d975816e3d4a14a2de0a595707d5bd019
rikiya miyazawa <rky.weed.rss@gmail.com>
2022-12-27 11:21:50 +0900

    feat:投稿、編集機能を作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i(show )
+  before_action :set_task, only: %i(show edit update)
   def index
     @tasks = Task.all
   end
@@ -8,9 +8,27 @@ class TasksController < ApplicationController
   end
   
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: 'タスクを追加しました'
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: 'タスクを編集しました'
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -22,5 +40,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
-  
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
 end
