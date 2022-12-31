8b9c39b1f16b94dea430b628a9d700601986fc6c
rikiya miyazawa <rky.weed.rss@gmail.com>
2022-12-31 16:42:20 +0900

    feat:タスクに終了期限登録機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,6 +44,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:title, :content)
+    params.require(:task).permit(:title, :content, :end_date)
   end
 end
