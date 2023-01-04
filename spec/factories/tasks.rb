b1efff40b72eda51ab7296e9f7795e9cc0d722ad
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-01-04 21:28:54 +0900

    test:検索機能のModel specを実装

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,9 +1,20 @@
 FactoryBot.define do
   factory :task do
-    # 下記の内容は実際に作成するカラム名に合わせて変更してください
     title { 'test_title' }
     content { 'test_content' }
     end_date { Date.new(2023, 1, 1) }
     start_status { 0 }
+  end
+  factory :second_task, class: Task do
+    title { 'test_title2' }
+    content { 'test_content2' }
+    end_date { Date.new(2023, 1, 2) }
+    start_status { 1 }
+  end
+  factory :third_task, class: Task do
+    title { 'test_title3' }
+    content { 'test_content3' }
+    end_date { Date.new(2023, 1, 3) }
+    start_status { 2 }
   end
 end
