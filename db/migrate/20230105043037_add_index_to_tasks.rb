19ec992e550e8709763c08668348c120fc8f9c40
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-01-06 13:58:34 +0900

    feat:ログイン機能作成のため、usersテーブルを作成

--- db/migrate/20230105043037_add_index_to_tasks.rb
+++ db/migrate/20230105043037_add_index_to_tasks.rb
@@ -1,5 +1,6 @@
 class AddIndexToTasks < ActiveRecord::Migration[6.0]
   def change
     add_index :tasks, [:title, :start_status]
+    add_index :users, :email, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_05_043037) do
+ActiveRecord::Schema.define(version: 2023_01_06_045430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,19 @@ ActiveRecord::Schema.define(version: 2023_01_05_043037) do
     t.date "end_date"
     t.integer "start_status", default: 0, null: false
     t.integer "priority", default: 0, null: false
+    t.bigint "user_id"
     t.index ["title", "start_status"], name: "index_tasks_on_title_and_start_status"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  add_foreign_key "tasks", "users"
 end
